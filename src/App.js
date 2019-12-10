05a5e1208db9c79ffd45e217db5cec4d7a874bc5
Gabriel Rabelo Camargos <rabelogabriel72@gmail.com>
2019-12-09 21:21:52 -0300

    Init features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 function App() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert("Aio");
+  };
   return (
-    <div className="App">
-      <h1>Ola mundo</h1>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Informe os valores dos resistores</h2>
+        <input type="number" name="r1" placeholder="Resistor 1" required />
+        <input type="number" name="r2" placeholder="Resistor 2" required />
+        <input type="number" name="r3" placeholder="Resistor 3" required />
+        <h2>Selecione o tipo de associação</h2>
+        <select name="type">
+          <option hidden required>
+            Selecione
+          </option>
+          <option>Série</option>
+          <option>Paralela</option>
+          <option>Mista</option>
+        </select>
+        <br />
+        <button type="submit">Calcular</button>
+      </form>
+    </>
   );
 }
 
